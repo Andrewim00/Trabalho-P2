b9bb658ac7fde3f725815cc9e204e497a4df975e
MatheusBetelli <127428784+MatheusBetelli@users.noreply.github.com>
2025-05-16 18:06:52 +0000

    Genero

---
+++ src/main/java/application/model/Genero.java
@@ -0,0 +1,23 @@
+package application.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "jogos")
+@Getter
+@Setter
+public class Genero {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+
+
+}
