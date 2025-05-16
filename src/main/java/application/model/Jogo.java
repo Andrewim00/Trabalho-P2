58df0deb48ad861f7efe7480d331e3744528d164
Andrewim00 <163583880+Andrewim00@users.noreply.github.com>
2025-05-16 18:39:05 +0000

    commit jogo

--- src/main/java/application/model/Jogo.java
+++ src/main/java/application/model/Jogo.java
@@ -1,9 +1,17 @@
 package application.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,5 +26,25 @@ public class Jogo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    
+    @Column(nullable = false)
+    private String titulo;
+
+    @ManyToMany
+    @JoinTable(name = "jogos_possuem_generos",
+        joinColumns = @JoinColumn(name = "id_jogo"),
+        inverseJoinColumns = @JoinColumn(name = "id_genero")
+    )
+    private Set<> generos = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(name = "jogos_possuem_generos",
+        joinColumns = @JoinColumn(name = "id_jogo"),
+        inverseJoinColumns = @JoinColumn(name = "id_plataforma")
+    )
+    private Set<> plataformas = new HashSet<>();
+
+    @OneToMany
+    @JoinColumn(name = "id_modo", nullable = false)
+    private long modo;
+
 }
